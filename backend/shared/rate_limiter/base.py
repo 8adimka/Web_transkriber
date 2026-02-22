c9fbfe439a1a072bbfb1627798879898e89ad52a
Vadim Medintsev <m8adimka@gmail.com>
2026-02-22 21:17:09 +0100

    rate_limiter_factory added

--- backend/shared/rate_limiter/base.py
+++ backend/shared/rate_limiter/base.py
@@ -1,5 +1,9 @@
 import random
+from functools import lru_cache
 from time import time
+from typing import Annotated
+
+from backend.shared.redis.base import get_redis
 
 
 class RateLimiter:
@@ -26,3 +30,30 @@ class RateLimiter:
             _, current_count, _, _ = res
 
             return current_count > max_requests
+
+
+@lru_cache
+def get_rate_limiter() -> RateLimiter:
+    return RateLimiter(get_redis())
+
+
+def rate_limiter_factory(
+    endpoint: str, max_requests: int, window_seconds: int
+) -> RateLimiter:
+    def dependency(
+        request: Request,
+        rate_limiter: Annotated[RateLimiter, Depends(get_rate_limiter)],
+    ):
+        ip_address = request.client.host
+
+        is_limited = await rate_limiter.is_limited(
+            ip_address, endpoint, max_requests, window_seconds
+        )
+
+        if is_limited:
+            raise HTTPException(
+                status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+                detail="Too Many Requests, please wait and try again later.",
+            )
+
+    return dependency
